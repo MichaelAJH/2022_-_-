f8b03a6c375deb89798dcfd150466d5542314758
Topologized <eternalbedrock@gmail.com>
2022-11-08 00:24:12 +0900

    processed data

--- data_open.py
+++ data_open.py
@@ -1,27 +1,27 @@
 import pandas as pd
+from datetime import date
 
 filename = './data/data.h5'
 orig_filename = "./data/data.csv"
 #변환중....
-
-
+"""
 store = pd.HDFStore(filename)
-df_reader = pd.read_csv(orig_filename, chunksize=256, encoding='euc-kr', iterator=True, header = 0, names = ["date", "code", "time", "start_id", "start_name", "end_id", "end_name", "total_n", "usage_time", "usage_dist"], usecols=["date", "code", "time", "start_id", "end_id", "total_n", "usage_time", "usage_dist"])
+df_reader = pd.read_csv(orig_filename, chunksize=65536, encoding='euc-kr', iterator=True, header = 0, names = ["date", "code", "time", "start_id", "start_name", "end_id", "end_name", "total_n", "usage_time", "usage_dist"], usecols=["date", "code", "time", "start_id", "end_id", "total_n", "usage_time", "usage_dist"])
+
+genesis = date(2021, 1, 1).toordinal()
 
 for chunk in df_reader:
-    for val in chunk['start_id']:
-        chunk = chunk.replace([val], int(val[3:]))
-    for val in chunk['end_id']:
-        chunk = chunk.replace([val], int(val[3:]))
-    print(chunk)
+    chunk["start_id"] = chunk["start_id"].map(lambda x: int(x[3:]))
+    chunk["end_id"] = chunk["end_id"].map(lambda x: int(x[3:]))
+    chunk["date"] = chunk["date"].map(lambda x: date(x // 10000, (x % 10000) // 100, x % 100).toordinal() - genesis)
     store.append('data', chunk)
 
 store.close()
 """
 
 s = pd.read_csv(orig_filename, nrows=5, encoding='euc-kr', header = 0, names = ["date", "code", "time", "start_id", "start_name", "end_id", "end_name", "total_n", "usage_time", "usage_dist"], usecols=["date", "code", "time", "start_id", "end_id", "total_n", "usage_time", "usage_dist"])
-print(s["end_id"][0])
-"""
+print(s)
+
 # def extract(row):
 #     row.start_id = row.start_id[3:]
 #     return row
